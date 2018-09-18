cd11b06a0045e7efc1ceb2478b5ef1fa1035b4ba
DMoon <dmoon.t@gmail.com>
2018-09-19 03:48:21 +0800

    Integrate CodePush

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
+import codePush from "react-native-code-push";
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const CodePushApp = codePush({
+  // options
+  updateDialog: false
+})(App);
+
+AppRegistry.registerComponent(appName, () => CodePushApp);
