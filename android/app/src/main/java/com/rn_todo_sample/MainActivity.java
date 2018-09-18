34a46e2722da785bef2bc32424651863f9499eee
DMoon <dmoon.t@gmail.com>
2018-09-18 10:16:50 +0800

    setup android

--- android/app/src/main/java/com/rn_todo_sample/MainActivity.java
+++ android/app/src/main/java/com/rn_todo_sample/MainActivity.java
@@ -1,8 +1,14 @@
 package com.rn_todo_sample;
-
+import android.content.Intent;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- android/app/src/main/java/com/rn_todo_sample/MainApplication.java
+++ android/app/src/main/java/com/rn_todo_sample/MainApplication.java
@@ -2,7 +2,11 @@ package com.rn_todo_sample;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -14,6 +18,11 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -24,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+          new FBSDKPackage(mCallbackManager),
+          new VectorIconsPackage()
       );
     }
 
@@ -42,6 +52,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
